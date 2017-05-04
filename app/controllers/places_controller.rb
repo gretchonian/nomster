4012dda8bb598184412473a882b874851c047ae2
Gretchen <gretchonian@hotmail.com>
2017-05-04 16:57:33 +0000

    Added Security

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 10)
@@ -12,8 +12,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -26,16 +30,31 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
+
+   
   end
 
   def update
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
-    redirect_to root_path
+     if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
   end
 
   def destroy
     @place = Place.find(params[:id])
+     if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+      end
     @place.destroy
     redirect_to root_path
   end
