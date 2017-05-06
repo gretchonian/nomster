959ae2ca7c62fc436619cbfc400f17625b58af46
Gretchen <gretchonian@hotmail.com>
2017-05-06 15:28:14 +0000

    Added User Comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :address, :description, presence: true
   geocoded_by :address
