790700d74c9725e6444fbe546c2edfe56c9daa79
Gretchen <gretchonian@hotmail.com>
2017-05-12 21:08:15 +0000

    Added Cont. Photos

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :address, :description, presence: true
   geocoded_by :address
