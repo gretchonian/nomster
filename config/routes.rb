0465ae603d9056b1ee3b2e071ccd30d13abbd8ac
Gretchen <gretchonian@hotmail.com>
2017-05-24 19:29:38 +0000

    Added User

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :comments, only: :create
     resources :photos, only: :create
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
 end
+
