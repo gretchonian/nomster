3174e56349f68e6909f22bf5d82357440953f920
Gretchen <gretchonian@hotmail.com>
2017-07-14 20:49:36 +0000

    Cleanup

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,36 +1,18 @@
 class PhotosController < ApplicationController
 before_action :authenticate_user!
-#params.require(:photo).permit(:caption, {pictures: []})
-#params.require(:user).permit(:email, :first_name, :last_name, {avatars: []})
 
   def create
       @place = Place.find(params[:place_id])
       @place.photos.create(photo_params)
       redirect_to place_path(@place)
-#u = User.new
-#u.avatar = params[:file] # Assign a file like this, or
-
-# like this
-#File.open('somewhere') do |f|
- # u.avatar = f
-#end
-
-#u.save!
-#u.avatar.url # => '/url/to/file.png'
-#u.avatar.current_path # => 'path/to/file.png'
-#u.avataSr_identifier # => 'file.png'
-#Note: u.avatar will never return nil, even if there is no photo associated to it. To check if a photo was saved to the model, use u.avatar.file.nil? instead.
-    #You can use your uploader class to store and retrieve files like this:
-
   end
 
+  private
 
-private
-
-def photo_params
-  params.require(:photo).permit(:picture, :caption)
-   #params.require(:comment).permit(:message, :rating)
-end
+  def photo_params
+    params.require(:photo).permit(:picture, :caption)
+     #params.require(:comment).permit(:message, :rating)
+  end
 
 end
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,6 @@ class PlacesController < ApplicationController
     @places = Place.paginate(:page => params[:page], :per_page => 10)
   end
 
-
   def new
     @place = Place.new
   end
@@ -32,31 +31,26 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-
-   
   end
 
   def update
     @place = Place.find(params[:id])
-
     if @place.user != current_user
-        return render text: 'Not Allowed', status: :forbidden
+      return render text: 'Not Allowed', status: :forbidden
     end
-
     @place.update_attributes(place_params)
-     if @place.valid?
+    if @place.valid?
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
-
   end
 
   def destroy
     @place = Place.find(params[:id])
-     if @place.user != current_user
-        return render text: 'Not Allowed', status: :forbidden
-      end
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
